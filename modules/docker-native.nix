145b7789814ca6c90f609dd1759e4e30c1138439
Dusk Banks <me@bb010g.com>
2022-12-10 02:05:52 +0000

    docker-native: normalize formatting

--- modules/docker-native.nix
+++ modules/docker-native.nix
@@ -1,34 +1,36 @@
 { config, lib, pkgs, ... }:
-with builtins; with lib; {
+let
+  cfg = config.wsl.docker-native;
+in
+{
+  options = {
+    wsl.docker-native = {
+      enable = lib.mkEnableOption "Native Docker integration in NixOS.";
 
-  options.wsl.docker-native = with types; {
-    enable = mkEnableOption "Native Docker integration in NixOS.";
+      addToDockerGroup = lib.mkOption {
+        type = lib.types.bool;
+        default = config.security.sudo.wheelNeedsPassword;
+        description = ''
+          Wether to add the default user to the docker group.
 
-    addToDockerGroup = mkOption {
-      type = bool;
-      default = config.security.sudo.wheelNeedsPassword;
-      description = ''
-        Wether to add the default user to the docker group.
-
-        This is not recommended, if you have a password, because it essentially permits unauthenticated root access.
-      '';
+          This is not recommended, if you have a password, because it essentially permits unauthenticated root access.
+        '';
+      };
     };
   };
 
-  config =
-    let
-      cfg = config.wsl.docker-native;
-    in
-    mkIf (config.wsl.enable && cfg.enable) {
-      environment.systemPackages = with pkgs; [
-        docker-compose
-      ];
+  config = lib.mkIf (config.wsl.enable && cfg.enable) {
+    environment.systemPackages = [
+      pkgs.docker-compose
+    ];
 
-      virtualisation.docker.package = (pkgs.docker.override { iptables = pkgs.iptables-legacy; });
-      virtualisation.docker.enable = true;
-
-      users.groups.docker.members = lib.mkIf cfg.addToDockerGroup [
-        config.wsl.defaultUser
-      ];
+    virtualisation.docker.package = pkgs.docker.override {
+      iptables = pkgs.iptables-legacy;
     };
+    virtualisation.docker.enable = true;
+
+    users.groups.docker.members = lib.mkIf cfg.addToDockerGroup [
+      config.wsl.defaultUser
+    ];
+  };
 }
